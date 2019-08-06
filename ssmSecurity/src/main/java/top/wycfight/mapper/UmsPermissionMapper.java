167c987e7408991d8d66c372b4b85c67b419d1f5
achunCoding <wycfight@163.com>
2019-08-06 09:07:09 +0800

    生成权限对应实体类，将jackson升级到2.9.9版本

---
+++ ssmSecurity/src/main/java/top/wycfight/mapper/UmsPermissionMapper.java
@@ -0,0 +1,30 @@
+package top.wycfight.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import top.wycfight.entity.UmsPermission;
+import top.wycfight.entity.UmsPermissionExample;
+
+public interface UmsPermissionMapper {
+    long countByExample(UmsPermissionExample example);
+
+    int deleteByExample(UmsPermissionExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsPermission record);
+
+    int insertSelective(UmsPermission record);
+
+    List<UmsPermission> selectByExample(UmsPermissionExample example);
+
+    UmsPermission selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsPermission record, @Param("example") UmsPermissionExample example);
+
+    int updateByExample(@Param("record") UmsPermission record, @Param("example") UmsPermissionExample example);
+
+    int updateByPrimaryKeySelective(UmsPermission record);
+
+    int updateByPrimaryKey(UmsPermission record);
+}
---
+++ ssmSecurity/src/main/java/top/wycfight/mapper/UmsRolePermissionRelationMapper.java
@@ -0,0 +1,30 @@
+package top.wycfight.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import top.wycfight.entity.UmsRolePermissionRelation;
+import top.wycfight.entity.UmsRolePermissionRelationExample;
+
+public interface UmsRolePermissionRelationMapper {
+    long countByExample(UmsRolePermissionRelationExample example);
+
+    int deleteByExample(UmsRolePermissionRelationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsRolePermissionRelation record);
+
+    int insertSelective(UmsRolePermissionRelation record);
+
+    List<UmsRolePermissionRelation> selectByExample(UmsRolePermissionRelationExample example);
+
+    UmsRolePermissionRelation selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsRolePermissionRelation record, @Param("example") UmsRolePermissionRelationExample example);
+
+    int updateByExample(@Param("record") UmsRolePermissionRelation record, @Param("example") UmsRolePermissionRelationExample example);
+
+    int updateByPrimaryKeySelective(UmsRolePermissionRelation record);
+
+    int updateByPrimaryKey(UmsRolePermissionRelation record);
+}
